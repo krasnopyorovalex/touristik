9d7dd143390db8a089db8dd22186c4396ba18381
Alex Krasnopyorov <krasnopyorovalex88@gmail.com>
2019-01-06 12:13:56 +0300

    add develop folders

---
+++ gulpfile.js
@@ -0,0 +1,120 @@
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const browserSync = require('browser-sync').create();
+const gcmq = require('gulp-group-css-media-queries');
+const preproc = require('gulp-stylus');
+const rename = require('gulp-rename');
+const type_utils = require('stylus-type-utils');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const svgSprite = require("gulp-svg-sprites");
+const nib = require('nib');
+const spritesmith = require('gulp.spritesmith');
+const merge = require('merge-stream');
+const babel = require('gulp-babel');
+
+const config = {
+    src: './src',
+    css: {
+        watch: '/precss/**/*.styl',
+        src: '/precss/app.styl',
+        dest: '/css'
+    },
+    js: {
+        src: '/prejs/*.js',
+        dest: '/js'
+    },
+    sprite: {
+        img: '/sprites/*.*',
+        outCss: '/precss',
+        outImg: '/img'
+    },
+    spriteSvg: {
+        img: '/icons-svg/*.svg',
+        outCss: '/precss',
+        outImg: '/img'
+    },
+    html: {
+        src: '/*.html'
+    }
+};
+
+gulp.task('build', function () {
+    gulp.src(config.src + config.css.src)
+            .pipe(preproc({
+                use:[
+                    nib(),
+                    type_utils()
+                ]
+            }))
+            .pipe(gcmq())
+            .pipe(autoprefixer({
+                browsers: ['> 0.1%'],
+                cascade: false
+            }))
+            .pipe(cleanCSS({
+                level: 2
+            }))
+            .pipe(rename({suffix: '.min'}))
+            .pipe(gulp.dest(config.src + config.css.dest))
+            .pipe(browserSync.reload({
+                stream: true
+            }));
+});
+
+gulp.task('js', function(){
+    return gulp.src(config.src + config.js.src)
+        // .pipe(babel({
+        //     presets: ['@babel/env']
+        // }))
+        .pipe(concat("app.js"))
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(config.src + config.js.dest))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task('watch', ['browserSync'], function () {
+    gulp.watch(config.src + config.css.watch, ['build']);
+    gulp.watch(config.src + config.js.src, ['js']);
+    gulp.watch(config.src + config.html.src, browserSync.reload);
+});
+
+gulp.task('browserSync', function () {
+    browserSync.init({
+        server: {
+            baseDir: config.src
+        }
+    });
+});
+
+gulp.task('sprites', function () {
+    return gulp.src(config.src + config.spriteSvg.img)
+        .pipe(svgSprite({
+            mode: "symbols"
+        }))
+        .pipe(gulp.dest(config.src + config.spriteSvg.outCss));
+});
+
+gulp.task('sprite', function () {
+    // Generate our spritesheet
+    var spriteData = gulp.src(config.src + config.sprite.img).pipe(spritesmith({
+        imgName: 'sprite.png',
+        cssName: 'sprite.styl',
+        padding: 10
+    }));
+
+    // Pipe image stream through image optimizer and onto disk
+    var imgStream = spriteData.img
+        .pipe(gulp.dest(config.src + config.sprite.outImg));
+
+    // Pipe CSS stream through CSS optimizer and onto disk
+    var stylStream = spriteData.css
+        .pipe(gulp.dest(config.src + config.sprite.outCss));
+
+    // Return a merged stream to handle both `end` events
+    return merge(imgStream, stylStream);
+});
---
+++ src/prejs/app.js
@@ -0,0 +1,203 @@
+jQuery(document).ready(function() {
+
+    lightbox.option({
+        'albumLabel': 'Изображение %1 из %2'
+    });
+
+    var owl = jQuery('.owl-carousel');
+    if (owl.length) {
+        owl.owlCarousel({
+            loop:false,
+            margin:0,
+            nav:false,
+            dots:false,
+            items: 1
+        });
+        var dots = jQuery('.owl-dots');
+        dots.on("click", ".owl-dot", function () {
+            var pos = parseInt(jQuery(this).text());
+            return owl.trigger('to.owl.carousel', pos - 1);
+        });
+    }
+
+    var callPopup = jQuery(".call__popup");
+    if(callPopup.length) {
+        var popup = jQuery(".popup"),
+            popupBg = jQuery(".popup__show-bg");
+        callPopup.on("click", function (e) {
+            e.preventDefault();
+            return popup.fadeIn() && popupBg.show();
+        });
+        popup.on("click", ".close", function () {
+            return popup.fadeOut("slow") && popupBg.fadeOut();
+        });
+    }
+
+    /*
+    |-----------------------------------------------------------
+    |   NoUiSlider
+    |-----------------------------------------------------------
+    */
+    var keypressSlider = document.getElementById('keypress');
+    var input0 = document.getElementById('input__price-from');
+    var input1 = document.getElementById('input__price-to');
+    var inputs = [input0, input1];
+
+    noUiSlider.create(keypressSlider, {
+        connect: true,
+        start: [0, 20000],
+        format: {
+            to: function (value) {
+                return parseInt(value);
+            },
+            from: function (value) {
+                return parseInt(value);
+            }
+        },
+        range: {
+            'min': [0],
+            'max': [20000]
+        }
+    });
+
+    keypressSlider.noUiSlider.on('update', function (values, handle) {
+        inputs[handle].value = values[handle];
+    });
+
+    inputs.forEach(function (input, handle) {
+
+        input.addEventListener('change', function () {
+            keypressSlider.noUiSlider.setHandle(handle, this.value);
+        });
+
+        input.addEventListener('keydown', function (e) {
+
+            var values = keypressSlider.noUiSlider.get();
+            var value = Number(values[handle]);
+
+            // [[handle0_down, handle0_up], [handle1_down, handle1_up]]
+            var steps = keypressSlider.noUiSlider.steps();
+
+            // [down, up]
+            var step = steps[handle];
+
+            var position;
+
+            // 13 is enter,
+            // 38 is key up,
+            // 40 is key down.
+            switch (e.which) {
+
+                case 13:
+                    keypressSlider.noUiSlider.setHandle(handle, this.value);
+                    break;
+
+                case 38:
+
+                    // Get step to go increase slider value (up)
+                    position = step[1];
+
+                    // false = no step is set
+                    if (position === false) {
+                        position = 1;
+                    }
+
+                    // null = edge of slider
+                    if (position !== null) {
+                        keypressSlider.noUiSlider.setHandle(handle, value + position);
+                    }
+
+                    break;
+
+                case 40:
+
+                    position = step[0];
+
+                    if (position === false) {
+                        position = 1;
+                    }
+
+                    if (position !== null) {
+                        keypressSlider.noUiSlider.setHandle(handle, value - position);
+                    }
+
+                    break;
+            }
+        });
+    });
+
+    var filterPanel = jQuery(".filter__panel");
+    if(filterPanel.length) {
+
+        filterPanel.find(".filter__block:not(.is__opened) .list").removeClass("hidden").hide();
+
+        filterPanel.on('click', '.label', function () {
+            var _this = jQuery(this),
+                filterBlock = _this.next('div').closest(".filter__block");
+            if(filterBlock.hasClass("is__opened")){
+                return filterBlock.removeClass("is__opened") && _this.next('div').slideToggle();
+            }
+            return filterBlock.addClass("is__opened") && _this.next('div').slideToggle();
+        });
+    }
+
+    /*
+    |-----------------------------------------------------------
+    |   notification
+    |-----------------------------------------------------------
+    */
+    var Notification = {
+        element: false,
+        setElement: function (element) {
+            return this.element = element;
+        },
+        notify: function (message) {
+            if( ! this.element) {
+                this.setElement(jQuery(".notify"));
+            }
+            return this.element.html('<div>' + message + '</div>') && this.element.fadeIn().delay(7000).fadeOut();
+        }
+    };
+
+    formHandler("#order__service-form", Notification);
+});
+
+function formHandler(selector, Notification, callPopup) {
+    return jQuery(document).on("submit", selector, function(e){
+        e.preventDefault();
+        var _this = jQuery(this),
+            url = _this.attr('action'),
+            data = _this.serialize(),
+            submitBlock = _this.find(".submit__block"),
+            agree = _this.find(".agree__block input[type=checkbox]");
+        if (agree.length && ! agree.prop("checked")) {
+            agree.parent(".agree__block").find(".error").fadeIn().delay(3000).fadeOut();
+            return false;
+        }
+        return jQuery.ajax({
+            type: "POST",
+            dataType: "json",
+            url: url,
+            data: data,
+            beforeSend: function(jqXHR, settings) {
+                return submitBlock.addClass("is__sent");
+            },
+            success: function (data) {
+                if(typeof callPopup !== "undefined" && callPopup.length) {
+                    jQuery(".popup").fadeOut("slow") && jQuery(".popup__show-bg").fadeOut();
+                }
+                Notification.notify(data.message);
+                return submitBlock.removeClass("is__sent") && _this.trigger("reset");
+            }
+        });
+    });
+}
+
+jQuery(document).ajaxError(function () {
+    return jQuery("form .submit__block").removeClass("is__sent") && jQuery('.notify').html('<div>Произошла ошибка =(</div>').fadeIn().delay(3000).fadeOut();
+});
+
+// Number.prototype.format = function(n, x) {
+//     var re = '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\.' : '$') + ')';
+//     return this.toFixed(Math.max(0, ~~n)).replace(new RegExp(re, 'g'), '$& ');
+// };
